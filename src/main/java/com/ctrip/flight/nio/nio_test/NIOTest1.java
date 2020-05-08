cc5d6951b44219837a3a18bad615fdac7036ccfe
xuke <kexu@mail.ustc.edu.cn>
2020-05-09 00:43:12 +0800

    modify test1

--- src/main/java/com/ctrip/flight/nio/nio_test/NIOTest1.java
+++ src/main/java/com/ctrip/flight/nio/nio_test/NIOTest1.java
@@ -17,7 +17,7 @@ import java.security.SecureRandom;
  * 装饰模式
  */
 /**
- * 快捷键：快速关闭ternimal：shift+esc
+ * 快捷键：快速关闭terminal：shift+esc
  *         IDEA点错了默认方法，如：buffer.position(),发现我们想点的是limit()方法，
  *         此时可以直接光标顶到buffer.这里，然后找到要的方法，直接tab键，后面的position方法将自动删除,省去了手动删除。
  */
@@ -60,11 +60,13 @@ public class NIOTest1 {
              int random = new SecureRandom().nextInt(20);
              intBuffer.put(random);
         }
+        System.out.println("在flip之前，position的值：" + intBuffer.position());
         System.out.println("在flip之前，limit的值：" + intBuffer.limit());
         //每次先往缓冲区中写数据之后，紧接着完成缓存区读操作的话，我们都得对缓冲区执行flip()方法，其目的是为了实现状态的翻转
         // flip方法里面维护了三个整数值：0 <= mark <= position <= limit <= capacity(mark=-1的时候表示mark标记失效)
         // mark用来和reset搭配使用，比如我读到第5个元素的时候，这时候执行reset操作，下一次我在读的时候则从mark标记的位置开始读
-        //刚开始初始化的时候，position初始化为0，capacity和limit的值都指向最后一个元素所在位置的下一个位置，mark暂时不用，每次往IntBuffer里写的时候只动position的值，往右移动
+        //刚开始初始化的时候，position初始化为0，capacity和limit的值都指向容量大小(从0开始计算的话，就等于IntBuffer的长度)，
+        // mark暂时不用，每次往IntBuffer里写的时候只动position的值，往右移动
         //在flip之后，limit指向position所指向的位置，即指向的是最后一个元素所在的索引的下一个索引，而position的值重新指向0.
         // clear方法完成的是将position、limit和capacity回到最初的状态，即position=0，limit = capacity，但是里面的元素其实是不会被清空的，仍然会保留下来
         intBuffer.flip();
